a427ca8e06c69bc317b0d697d66ab8b1084a7aac
Ainara <ainarabliz@gmail.com>
2023-11-01 23:59:47 +0100

    keylistener

--- Maven20/src/main/java/ejercicio8/ConvertidorPro.java
+++ Maven20/src/main/java/ejercicio8/ConvertidorPro.java
@@ -1,17 +1,20 @@
-package ejercicio7;
+package ejercicio8;
 
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Convertidor extends JFrame {
+public class ConvertidorPro extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -21,15 +24,15 @@ public class Convertidor extends JFrame {
 	private JTextField apts;
 	private JTextField a€;
 	private JTextField cantidad;
+	private JButton borrar; 
 
-	public Convertidor() {
-		setTitle("Convertidor de pts.- €");
-		setBounds(650, 300, 500, 200);
+	public ConvertidorPro() {
+		setTitle("Convertidor pro de pts.- €");
+		setBounds(650, 300, 500, 250);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
 		contentPane = new JPanel();
 		contentPane.setLayout(null);
-		setContentPane(contentPane);
 		System.out.println(contentPane);
 
 		convertir = new JLabel();
@@ -44,32 +47,32 @@ public class Convertidor extends JFrame {
 		€_pts.setFont(new Font("Tahoma", Font.BOLD, 11));
 		€_pts.setForeground(new Color(230, 230, 250));
 		€_pts.setBackground(new Color(0, 128, 0));
-		€_pts.setBounds(258, 87, 89, 23);
+		€_pts.setBounds(258, 105, 89, 23);
 		contentPane.add(€_pts);
-		
+
 		pts_€ = new JButton("pts. - €");
 		pts_€.setFont(new Font("Tahoma", Font.BOLD, 11));
 		pts_€.setForeground(new Color(230, 230, 250));
 		pts_€.setBackground(new Color(0, 128, 0));
-		pts_€.setBounds(23, 87, 89, 23);
+		pts_€.setBounds(23, 105, 89, 23);
 		contentPane.add(pts_€);
-		
+
 		apts = new JTextField();
 		apts.setEditable(false);
 		apts.setForeground(new Color(0, 102, 0));
 		apts.setFont(new Font("Tahoma", Font.BOLD, 12));
-		apts.setBounds(357, 87, 106, 23);
+		apts.setBounds(358, 108, 106, 20);
 		contentPane.add(apts);
 		apts.setColumns(10);
-		
+
 		a€ = new JTextField();
 		a€.setEditable(false);
 		a€.setForeground(new Color(0, 102, 0));
 		a€.setFont(new Font("Tahoma", Font.BOLD, 12));
-		a€.setBounds(122, 87, 107, 23);
+		a€.setBounds(122, 108, 107, 20);
 		contentPane.add(a€);
 		a€.setColumns(10);
-		
+
 		cantidad = new JTextField();
 		cantidad.setForeground(new Color(0, 102, 0));
 		cantidad.setFont(new Font("Tahoma", Font.BOLD, 12));
@@ -77,26 +80,89 @@ public class Convertidor extends JFrame {
 		contentPane.add(cantidad);
 		cantidad.setColumns(10);	
 
+		borrar = new JButton("Borrar");
+		borrar.setForeground(new Color (230, 230, 250));
+		borrar.setFont(new Font("Tahoma", Font.BOLD, 11));
+		borrar.setBackground(new Color(0, 102, 0));
+		borrar.setBounds(202, 153, 89, 23);
+		contentPane.add(borrar);
+
 		ActionListener al1=new ActionListener(){ 
 			@Override
 			public void actionPerformed(ActionEvent e) {	
-				double qtity = Double.parseDouble(cantidad.getText());
-				double result;
-				result =(qtity * 166.386);
-			    apts.setText(String.format("%.2f", result));
+				apesetas();
 			}
 		};
 		€_pts.addActionListener(al1);
-		
+
 		ActionListener al2=new ActionListener(){ 
 			@Override
 			public void actionPerformed(ActionEvent e) {	
-				double qtity = Double.parseDouble(cantidad.getText());
-				double result;
-				result = (qtity / 166.386);
-			    a€.setText(String.format("%.2f", result));
+				aeuros();
 			}
 		};
 		pts_€.addActionListener(al2);
+
+		ActionListener clear=new ActionListener(){ 
+			@Override
+			public void actionPerformed(ActionEvent e) {	
+				vaciar();
+			}
+		};
+		borrar.addActionListener(clear);
+
+		addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				if(arg0.getKeyCode()==KeyEvent.VK_ENTER) {
+					
+				}
+			}
+
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
+		setContentPane(contentPane);
 	}
+	/*€_pts.addKeyListener(null);
+		pts_€.addKeyListener(null);*/
+
+	public void aeuros() {
+		try {
+			double qtity = Double.parseDouble(cantidad.getText());
+			double result;
+			result = (qtity / 166.386);
+			a€.setText(String.format("%.2f", result));
+		} catch (Exception e2) {
+			JOptionPane.showMessageDialog(null, "Error. Introduzca bien la cantidad a convertir.");
+		}
+	}
+	public void apesetas() {
+		try {
+			double qtity = Double.parseDouble(cantidad.getText());
+			double result;
+			result =(qtity * 166.386);
+			apts.setText(String.format("%.2f", result));
+		} catch (Exception e2) {
+			JOptionPane.showMessageDialog(null, "Error. Introduzca bien la cantidad a convertir.");
+		}
+	}
+	public void vaciar() {
+		apts.setText("");
+		a€.setText("");
+		cantidad.setText("");
+	}
+
 }
